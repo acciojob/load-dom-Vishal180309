3cbfed20ff0ebe6a7e1af69ca05e75c2694794aa
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-03-01 08:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
     const textNode = document.createTextNode("DOM load success");
     document.body.appendChild(textNode);
